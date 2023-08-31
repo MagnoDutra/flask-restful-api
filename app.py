cc70b9613307b14c3bf291bb1531df5376f88d4d
MagnoDutra <jmagno.dutra@hotmail.com>
2023-08-30 23:01:19 -0300

    criado modelo usuario

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
+from resources.usuario import Usuario
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
@@ -15,6 +16,7 @@ def criar_banco():
 
 api.add_resource(Hoteis, "/hoteis")
 api.add_resource(Hotel, "/hoteis/<string:id>")
+api.add_resource(Usuario, "/usuario/<string:id>")
 
 if __name__ == "__main__":
     from sql_alchemy import banco
---
+++ resources/usuario.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource
+
+
+class Usuario(Resource):
+    def get(self, id):
+        pass
+
+    def delete(self, id):
+        pass
